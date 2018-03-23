3b6342bad64f7a9f74d4cdcb8f5754b46b76c923
johnjohnx4 <johnjohnx4@gmmail.com>
2018-03-23 12:05:12 -0400

    completed password hashing and comparing

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -1,30 +1,39 @@
+import { callbackify } from 'util';
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const SALT_ROUNDS = 11;
 
-const UserSchema = Schema({
-  // create your user schema here.
-  // username: required, unique and lowercase
-  // password: required
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  password: {
+    type: string,
+    required: true
+  }
 });
 
 UserSchema.pre('save', function(next) {
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-  // if there is an error here you'll need to handle it by calling next(err);
-  // Once the password is encrypted, call next() so that your userController and create a user
+  bcrypt.pre('save', next => {
+    bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+      if(err) throw new Error(err);
+      user.password = hash;
+      next();
+    })
+  })
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // Fill this method in with the Proper password comparing, bcrypt.compare()
-  // Your controller will be responsible for sending the information here for password comparison
-  // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  bcrypt.compare(plainTextPW, this.password, (err, match) => {
+    if (err) return callBack(err);
+    callBack(null, match);
+  })
 };
-
-// if you're really stuck with this at this point, you can reference this document.
-// https://github.com/LambdaSchool/Auth-JWT/blob/master/models/index.js This is what we're going for here.
 
 module.exports = mongoose.model('User', UserSchema);

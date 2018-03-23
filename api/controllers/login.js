6e1f3a89a82b9faa60d05de4a5c7d9aa4f1903fb
johnjohnx4 <johnjohnx4@gmmail.com>
2018-03-23 12:55:05 -0400

    completed sprint, moving on to stretch

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -15,7 +15,6 @@ const login = (req, res) => {
       return;
     }
     user.checkPassword(password, (nonMatch, hashMatch) => {
-      // This is an example of using our User.method from our model.
       if (nonMatch !== null) {
         res.status(422).json({ error: 'passwords dont match' });
         return;
@@ -23,9 +22,9 @@ const login = (req, res) => {
       if (hashMatch) {
         const payload = {
           username: user.username
-        }; // what will determine our payload.
-        const token = jwt.sign(payload, mysecret); // creates our JWT with a secret and a payload and a hash.
-        res.json({ token }); // sends the token back to the client
+        };
+        const token = jwt.sign(payload, mysecret);
+        res.json({ token });
       }
     });
   });
